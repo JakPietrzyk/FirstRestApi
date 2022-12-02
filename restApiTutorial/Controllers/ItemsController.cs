bed22e3ac4d9e7d54cf98994fc71d1eb92244158
Spektra Kowalski <pietrzykj9@gmail.com>
2022-12-02 19:30:52 +0100

    baseApi

---
+++ restApiTutorial/Controllers/ItemsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using restApiTutorial.Entities;
+using restApiTutorial.Repositories;
+
+namespace restApiTutorial.Controllers
+{
+    [ApiController]
+    [Route ("items")]
+    public class ItemsController: ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Entities.Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+
+            if(item==null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+    }
+}
